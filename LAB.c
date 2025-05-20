4c54dfe9bf618d81450fcbbbd1381ad9e27f3e38
Davos <58948657+Elements2720@users.noreply.github.com>
2025-05-21 00:41:48 +0300

    Update LAB.c

--- LAB.c
+++ LAB.c
@@ -12,7 +12,7 @@ void main () {
 	}
 }
 
-//Q2 PULL DOWN RESISTOR SWITCH
+//Q2 PULL-DOWN RESISTOR SWITCH
 
 void main() {
     TRISC.F5 = 0; // LED Output
@@ -40,4 +40,33 @@ void main() {
             PORTC.F5 = 1; // LED OFF when button is not pressed
         }
     }
+}
+
+
+//Q4 7-Segmant from 0 ==> 9 without decoder
+
+void main() {
+  TRISB = 0b10000000;
+  for (;;) {
+    PORTB = 0b00111111;  // 0
+    delay_ms(500);
+    PORTB = 0b00000110;  // 1
+    delay_ms(500);
+    PORTB = 0b01011011;  // 2
+    delay_ms(500);
+    PORTB = 0b01001111;  // 3
+    delay_ms(500);
+    PORTB = 0b01100110;  // 4
+    delay_ms(500);
+    PORTB = 0b01101101;  // 5
+    delay_ms(500);
+    PORTB = 0b01111101;  // 6
+    delay_ms(500);
+    PORTB = 0b00000111;  // 7
+    delay_ms(500);
+    PORTB = 0b01111111;  // 8
+    delay_ms(500);
+    PORTB = 0b01101111;  // 9
+    delay_ms(500);
+  }
 }
