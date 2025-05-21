e8900f66fa7faa1b5ffa691902a83a65e758d971
Davos <58948657+Elements2720@users.noreply.github.com>
2025-05-21 10:59:19 +0300

    Update LAB.c

--- LAB.c
+++ LAB.c
@@ -1,17 +1,12 @@
 //Q1 TOGGLE LED 
 void main () {
-	TRISC.F5 = 0;
-	TRISB.F1 = 1;
+	TRISB.F0 = 0;
 
-	while(1){
-		if (PORTB.F1 == 0) {
-			PORTC.F5 = 1;
-		} else {
-			PORTC.F5 = 0;
-		}
+	for (;;) {
+	    PORTB.F0 =~ PORTB.F0;
+            delay_ms(110);
 	}
 }
-
 //Q2 PULL-DOWN RESISTOR SWITCH
 
 void main() {
@@ -35,9 +30,9 @@ void main() {
 
      while(1) {
         if (PORTB.F4 == 1) {
-            PORTC.F5 = 0; // LED ON when button is pressed
+            PORTC.F5 = 0; // LED OFF when button is pressed
         } else {
-            PORTC.F5 = 1; // LED OFF when button is not pressed
+            PORTC.F5 = 1; // LED ON when button is not pressed
         }
     }
 }
